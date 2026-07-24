118c52413d6e3892a8ce047008ce2e403b03cc84
wallissonghost-code <wallissonghost@gmail.com>
2026-07-24 10:50:48 -0300

    Atualizar cache offline dos módulos

--- sw.js
+++ sw.js
@@ -1,5 +1,16 @@
-const CACHE = 'orcazap-v1';
-const ASSETS = ['/', '/index.html', '/styles.css', '/app.js', '/icon.svg', '/manifest.webmanifest'];
+const CACHE = 'orcazap-v2';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app-core.js',
+  '/app-views.js',
+  '/app-events.js',
+  '/app-quotes.js',
+  '/app-pdf.js',
+  '/icon.svg',
+  '/manifest.webmanifest'
+];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
 self.addEventListener('fetch', event => {
