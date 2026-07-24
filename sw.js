86e48f54182ca653dd472488569820cd61e028db
wallissonghost-code <wallissonghost@gmail.com>
2026-07-24 19:09:14 -0300

    Atualizar cache do PDF luxo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'orcazap-v7';
+const CACHE = 'orcazap-v8';
 const ASSETS = [
   '/',
   '/index.html',
@@ -9,6 +9,7 @@ const ASSETS = [
   '/app-quotes.js',
   '/app-pdf.js',
   '/pdf-fix.js?v=7',
+  '/pdf-luxury.js?v=8',
   '/icon.svg',
   '/manifest.webmanifest'
 ];
