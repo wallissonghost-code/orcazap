f2a49ad8df370a373e5a9a1679973996038e4e9b
wallissonghost-code <wallissonghost@gmail.com>
2026-07-24 21:57:04 -0300

    Forçar atualização para versão 15

--- app-update.js
+++ app-update.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (function forceOrcaZapUpdate() {
-  const BUILD = '14';
+  const BUILD = '15';
   const BUILD_KEY = 'orcazap:app-build';
   const RELOAD_KEY = 'orcazap:build-reloaded';
 
